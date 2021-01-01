89eca0beda6c1fc02a0e4b00d93112ec63cab2af
UaHaHa <frolovi97@mail.ru>
2021-01-01 17:02:57 +0300

    l2

---
+++ GB/src/l2/User.java
@@ -0,0 +1,41 @@
+package l2;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private long id;
+
+    public User(long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
